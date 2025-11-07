04ed511a90c0af53f1cedc6c247aeb7a6d21386a
Yaroslav Lytvyn <yaroslav_system.admin@newdevils.net>
2025-11-07 15:24:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8474753126-aa51a",
+  "appId": "1:366357516428:web:514fb3791cc1ca43b46ba9",
+  "apiKey": "AIzaSyC9wzpitv6aM6TjNqJWaM1FLMeR7y7iJvo",
+  "authDomain": "studio-8474753126-aa51a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "366357516428"
+};
