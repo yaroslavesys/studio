0540cbfabc8fb28685335aa36bc40f7ea2beda96
Yaroslav Lytvyn <yaroslav_system.admin@newdevils.net>
2025-11-07 17:14:48 +0000

    113 |   public static async create(context: SecurityRuleContext): Promis

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -108,7 +108,7 @@ export function useCollection<T = any>(
     return () => unsubscribe();
   }, [memoizedTargetRefOrQuery]); // Re-run if the target query/reference changes.
   if(memoizedTargetRefOrQuery && !memoizedTargetRefOrQuery.__memo) {
-    throw new Error(memoizedTargetRefOrQuery + ' was not properly memoized using useMemoFirebase');
+    throw new Error('useCollection was not properly memoized using useMemoFirebase on ' + (memoizedTargetRefOrQuery as any)?.path);
   }
   return { data, isLoading, error };
 }
